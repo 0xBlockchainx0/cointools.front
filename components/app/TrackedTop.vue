cc4c640a74583a7c9dcc2456cec5a488a862d415
RJS <premonday@gmail.com>
2021-03-15 20:40:58 +0000

    /

--- components/app/TrackedTop.vue
+++ components/app/TrackedTop.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sticky top-0 p-4 border-b bg-white z-50">
+  <div v-show="tracking.length > 0" class="sticky top-0 p-4 border-b bg-white z-50">
     <div class="container grid grid-flow-col auto-cols-auto gap-2">
       <card-market variant="top" v-for="item in tracking" :key="item" :object="item"></card-market>
     </div>
